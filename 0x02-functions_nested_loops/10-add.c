bc0d0d8f8e1a2efd4b05dcd430c7dff6aa7fede9
Peter-gift <giftpeterson2021@gmail.com>
2022-09-14 14:16:13 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - adds two integers
  * @n1 - integer 1
  * @n2 - integer 2
- * Description: returns sum of two integers
+ * Description: returns the sum of two integers
  * Return: sum
  */
 
@@ -12,7 +12,7 @@ int add(int n1, int n2)
 {
 	int sum;
 
-	sum = n1 + n1;
+	sum = n1 + n2;
 
 	return (sum);
 }
