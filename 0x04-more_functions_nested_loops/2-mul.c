1fe255d4bab1cf07457f198a6ebc61f59eddb3d7
Peter-gift <giftpeterson2021@gmail.com>
2022-09-15 07:47:38 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiples of two numbers
+* @num1: first number
+* @num2: second number
+* Return: returns result
+*/
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
